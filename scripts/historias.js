f38ca33f6bbe02258c3bd3ee2280c54eafb61a19
Noe Osorio <osorionoe@gmail.com>
2021-04-27 15:15:38 -0500

    Crear historia

--- scripts/historias.js
+++ scripts/historias.js
@@ -1,4 +1,25 @@
 function createStorie() {
+  const form = document.getElementById("storiesForm");
+  const data = {
+    title: form.elements["title"].value,
+    id: form.elements["storieId"].value,
+    description: form.elements["description"].value,
+    date: form.elements["date"].value,
+    score: form.elements["score"].value,
+    observations: form.elements["observations"].value,
+    time: form.elements["time"].value,
+    timeSelect:
+      document.querySelector('input[name="timeSelect"]:checked').value ||
+      "days",
+  };
+
+  paintStorie(data);
+
+  // TODO: Guardar en la base de datos
+  // saveStorie(data)
+}
+
+function paintStorie(data) {
   const stories = document.getElementById("stories");
   const col = document.createElement("div");
   col.classList.add("col-2", "me-4", "my-2");
@@ -11,19 +32,22 @@ function createStorie() {
 
   const title = document.createElement("h4");
   title.classList.add("card-title");
-  title.innerHTML = "Historia 1";
+  title.innerHTML = data.title || "";
 
   const date = document.createElement("span");
   date.classList.add("text-start");
-  date.innerHTML = new Date().toLocaleDateString();
+  console.log(data.date);
+  date.innerHTML = data.date || "Sin fecha";
 
   const storieId = document.createElement("p");
   storieId.classList.add("storie-id", "text-center", "my-5");
-  storieId.innerText = getRandomInt(1, 100).toString();
+  storieId.innerText = data.score || "Sin valor";
 
   const days = document.createElement("p");
   days.classList.add("fs-3", "text-center");
-  days.innerText = getRandomInt(10, 15).toString() + " días";
+  days.innerText = `${data.time} ${
+    data.timeSelect === "days" ? "Días" : "Semanas"
+  }`;
 
   body.appendChild(title);
   body.appendChild(date);
