b4c3caa1d750ea503b214ca0c56f8bc24fcd9303
Noe Osorio <osorionoe@gmail.com>
2021-04-27 23:44:16 -0500

    login vista

---
+++ scripts/login.js
@@ -0,0 +1,11 @@
+function login() {
+  const data = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+  };
+
+  console.log(data);
+  // TODO: Servicio
+  // CAmbiar a la url que es
+  //   window.location.replace("index.html");
+}
